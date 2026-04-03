39babef050d7ffa716a7d7bef3ddde37ea1123c7
Wix Remote Machine <support@wix.com>
2026-04-03 11:05:01 +0000

    Revert sitemap.xml

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -2,34 +2,46 @@ export async function GET() {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
-  <!-- Core -->
+  <!-- Core Pages -->
   <url><loc>https://www.chaseinfrench.com/</loc></url>
+  <url><loc>https://www.chaseinfrench.com/start</loc></url>
   <url><loc>https://www.chaseinfrench.com/about</loc></url>
+  <url><loc>https://www.chaseinfrench.com/a-propos</loc></url>
   <url><loc>https://www.chaseinfrench.com/contact</loc></url>
 
-  <!-- Services -->
-  <url><loc>https://www.chaseinfrench.com/french-classes</loc></url>
-  <url><loc>https://www.chaseinfrench.com/online-french-lessons</loc></url>
-  <url><loc>https://www.chaseinfrench.com/private-french-tutor</loc></url>
+  <!-- Learning Topics -->
+  <url><loc>https://www.chaseinfrench.com/topics</loc></url>
+  <url><loc>https://www.chaseinfrench.com/topics/songs</loc></url>
+  <url><loc>https://www.chaseinfrench.com/pronunciation</loc></url>
+  <url><loc>https://www.chaseinfrench.com/slang</loc></url>
+  <url><loc>https://www.chaseinfrench.com/grammar</loc></url>
+  <url><loc>https://www.chaseinfrench.com/culture</loc></url>
 
-  <!-- Courses -->
-  <url><loc>https://www.chaseinfrench.com/french-course-for-beginners</loc></url>
-  <url><loc>https://www.chaseinfrench.com/intermediate-french-course</loc></url>
+  <!-- Classes -->
+  <url><loc>https://www.chaseinfrench.com/classes</loc></url>
+  <url><loc>https://www.chaseinfrench.com/classes/group</loc></url>
+  <url><loc>https://www.chaseinfrench.com/classes/private</loc></url>
 
-  <!-- Niche SEO Pages -->
-  <url><loc>https://www.chaseinfrench.com/french-for-actors</loc></url>
-  <url><loc>https://www.chaseinfrench.com/french-pronunciation-coach</loc></url>
-  <url><loc>https://www.chaseinfrench.com/learn-french-fast</loc></url>
+  <!-- Lessons -->
+  <url><loc>https://www.chaseinfrench.com/lecons</loc></url>
 
-  <!-- Content / Blog -->
-  <url><loc>https://www.chaseinfrench.com/blog</loc></url>
-  <url><loc>https://www.chaseinfrench.com/blog/how-to-learn-french-fast</loc></url>
-  <url><loc>https://www.chaseinfrench.com/blog/french-pronunciation-tips</loc></url>
-  <url><loc>https://www.chaseinfrench.com/blog/common-french-mistakes</loc></url>
-
-  <!-- Podcast -->
+  <!-- Podcasts -->
   <url><loc>https://www.chaseinfrench.com/podcast</loc></url>
-  <url><loc>https://www.chaseinfrench.com/podcast/episode-1</loc></url>
+
+  <!-- Songs -->
+  <url><loc>https://www.chaseinfrench.com/songs</loc></url>
+  <url><loc>https://www.chaseinfrench.com/songs/french-songs-lab-11</loc></url>
+
+  <!-- Resources -->
+  <url><loc>https://www.chaseinfrench.com/links</loc></url>
+  <url><loc>https://www.chaseinfrench.com/diagnostics</loc></url>
+
+  <!-- Legal -->
+  <url><loc>https://www.chaseinfrench.com/privacy-policy</loc></url>
+  <url><loc>https://www.chaseinfrench.com/politique-confidentialite</loc></url>
+
+  <!-- Sitemap -->
+  <url><loc>https://www.chaseinfrench.com/sitemap</loc></url>
 
 </urlset>`;
 
